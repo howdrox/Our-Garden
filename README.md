b5588b31256f9c1cf9aacd0be9bcad4b47a9d9af
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2022-11-15 17:54:59 +0100

    indicator !

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ english or french variable names
 
 ## Usefull sites
 
+https://moodle.insa-lyon.fr/pluginfile.php/345540/mod_resource/content/1/SUJET%20-%20Etude%20InSavour%20-%20Interactions%20Savoureuses%20-%20v02.8.html
+
 http://monpotagerorg.herokuapp.com/
--- code/csvProcessor.py
+++ code/csvProcessor.py
@@ -1,16 +1,35 @@
 import csv,json
 
+def genFavorise():
+    favorise={}
+    with open("./csv/data_arcs_poids.csv") as arcs:
+        reader=csv.reader(arcs,delimiter=';')
+        for source, interaction, cible, poids in reader:
+            if interaction=='favorise':
+                if source in favorise: 
+                    favorise[source][cible] = int(poids)
+                else:
+                    favorise[source] = {cible:int(poids)}
 
-favorise={}
-with open("./csv/data_arcs_poids.csv") as arcs:
-    reader=csv.reader(arcs,delimiter=';')
-    for source, interaction, cible, poids in reader:
-        if interaction=='favorise':
-            if source in favorise: 
-                favorise[source][cible] = int(poids)
-            else:
-                favorise[source] = {cible:int(poids)}
+    with open('./json/favorise.json','w+') as f:
+        json.dump(favorise,f)
 
-with open('./json/favorise.json','w+') as f:
-    json.dump(favorise,f)
+def genIndicator():
+    indicator={}
+    with open("./csv/data_sommets_bioindicateurs.csv") as f:
+        reader=csv.reader(f,delimiter=';')
+        for espece,lumiere,temperature,humidite,pH,nutriment,texture,organique in reader:
+            indicator[espece]={
+                'lumiere':lumiere,
+                'temperature':temperature,
+                'humidite':humidite,
+                'pH':pH,
+                'nutriment':nutriment,
+                'texture':texture,
+                'organique':organique
+            }
 
+    with open('./json/indicator.json','w+') as f:
+        json.dump(indicator,f)
+
+genIndicator()
